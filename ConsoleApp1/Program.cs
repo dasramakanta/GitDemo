c8f27d975cfe8973edc81de88cc6690fb03db18c
dasramakanta <das.ramakanta9090718196@outlook.com>
2022-01-10 14:50:09 +0530

    first messege

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    
+
+
+    class office
+    {
+       
+    }
+    class Customer:office
+    {
+
+    }
+
+    interface Ioffice<T>
+    {
+
+    }
+
+
+
+   
+
+
+    
+
+
+
+    class MainClass
+    {
+        static void Main()
+        {
+            office of = new Customer();
+
+        }
+    }
+}
+    
+
